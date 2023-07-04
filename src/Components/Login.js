33ebc8a4f92746093094b49ccb94d7ec13de1c11
M4RT1N <mjlynch35@yahoo.com>
2023-07-04 16:28:03 -0500

    Updated login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 export default function Login({ setIsNavOpen }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [activeButton, setActiveButton] = useState("login");
+
   useEffect(() => {
-    setIsNavOpen(false); // Set isNavOpen to false when component mounts
+    setIsNavOpen(false);
+    setIsLoading(false);
   }, []);
+
+  const handleButtonClick = (button, event) => {
+    event.preventDefault(); // Prevent form submission
+
+    setActiveButton(button);
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div className="login">
       <form>
+        <div className="login-signup">
+          <button
+            className={`button-login ${
+              activeButton === "login" ? "active" : ""
+            }`}
+            onClick={(event) => handleButtonClick("login", event)}
+          >
+            Login
+          </button>
+          <button
+            className={`button-signup ${
+              activeButton === "signup" ? "active" : ""
+            }`}
+            onClick={(event) => handleButtonClick("signup", event)}
+          >
+            Sign Up
+          </button>
+        </div>
         <h1>Login</h1>
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
