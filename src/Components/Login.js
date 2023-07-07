7742d6cebd6d3174858c2716649a251a62163638
Reid M <reidmadock@gmail.com>
2023-07-07 11:32:03 -0500

    adjusted login/signup page for backend

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,9 +4,9 @@ import Loading from "./Loading";
 export default function Login({ setIsNavOpen }) {
   const [isLoading, setIsLoading] = useState(true);
   const [activeButton, setActiveButton] = useState("login");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordMatch, setPasswordMatch] = useState(false);
+  const [passwordEmpty, setPasswordEmpty] = useState(true);
+  const [formData, setFormData] = useState({ email: "", password: "", name: "", confirmPass: "" });
 
   useEffect(() => {
     setIsNavOpen(false);
@@ -15,20 +15,44 @@ export default function Login({ setIsNavOpen }) {
 
   const handleButtonClick = (button, event) => {
     event.preventDefault(); // Prevent form submission
+    // Clear the formData
+    setFormData({ email: "", password: "", name: "", confirmPass: "" });
+    // Set which is active
     setActiveButton(button);
   };
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleConfirmPasswordChange = (event) => {
-    setConfirmPassword(event.target.value);
+  const handleInput = (name, value) => {
+    setFormData({...formData, [name]: value});
   };
 
   useEffect(() => {
-    setPasswordMatch(password === confirmPassword);
-  }, [password, confirmPassword]);
+    setPasswordMatch(formData.password === formData.confirmPass);
+  }, [formData.password, formData.confirmPass]);
+
+  useEffect(() => {
+    setPasswordEmpty((formData.password === "" || formData.confirmPass === ""));
+  })
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    // add code here to check if we're logging in or signing up.
+    if(activeButton === "signup") {
+      console.log("signing up...");
+      try {
+
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      console.log("logging in...");
+      try {
+
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setFormData({ email: "", password: "", name: "", confirmPass: "" });
+  }
 
   if (isLoading) {
     return <Loading />;
@@ -36,7 +60,7 @@ export default function Login({ setIsNavOpen }) {
 
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <div className="login-signup">
           <button
             className={`button-login ${
@@ -57,67 +81,76 @@ export default function Login({ setIsNavOpen }) {
         </div>
         <h1>{activeButton === "signup" ? "Sign Up" : "Login"}</h1>
         {activeButton === "signup" && (
+          // Sign up form variant
           <>
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="Name" />
-            <div className="inputs">
-              <input
-                type="password"
-                placeholder="Password"
-                className={`pass-input ${
-                  !passwordMatch && password && confirmPassword ? "error" : ""
-                }`}
-                value={password}
-                onChange={handlePasswordChange}
-              />
-              {!passwordMatch && password && confirmPassword && (
-                <p
-                  style={{
-                    fontSize: "12px",
-                    color: "red",
-                  }}
-                >
-                  Passwords do not match
-                </p>
-              )}
-            </div>
-            <div className="inputs">
-              <input
-                type="password"
-                placeholder="Confirm Password"
-                className={`confirm-pass ${
-                  !passwordMatch && password && confirmPassword ? "error" : ""
-                }`}
-                value={confirmPassword}
-                onChange={handleConfirmPasswordChange}
-              />
-              {!passwordMatch && password && confirmPassword && (
-                <p
-                  style={{
-                    fontSize: "12px",
-                    color: "red",
-                  }}
-                >
-                  Passwords do not match
-                </p>
-              )}
-              {passwordMatch && password && confirmPassword && (
-                <p
-                  style={{
-                    fontSize: "12px",
-                    color: "green",
-                  }}
-                >
-                  Passwords match
-                </p>
-              )}
-            </div>
+            <input 
+              type="email" 
+              placeholder="Email" 
+              name="email" 
+              value={formData.email} 
+              onChange={(event) => handleInput('email', event.target.value)} 
+            />
+            <input 
+              type="text" 
+              placeholder="Name" 
+              name="name" 
+              value={formData.name} 
+              onChange={(event) => handleInput('name', event.target.value)} 
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className={`pass-input ${!(passwordMatch) ? "error" : ""}`}
+              value={formData.password}
+              onChange={(event) => handleInput('password', event.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className={`confirm-pass ${
+                !passwordMatch ? "error" : ""
+              }`}
+              value={formData.confirmPass}
+              onChange={(event) => handleInput('confirmPass', event.target.value)}
+            />
+            {(!passwordMatch) && (
+              <p
+                style={{
+                  fontSize: "12px",
+                  color: "red",
+                }}
+              >
+                Passwords do not match
+              </p>
+            )}
+            {(passwordMatch && !passwordEmpty) && (
+              <p
+                style={{
+                  fontSize: "12px",
+                  color: "green",
+                }}
+              >
+                Passwords match
+              </p>
+            )}
           </>
         )}
         {activeButton === "login" && (
+          // Login form variant
           <>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
+            <input 
+              type="email" 
+              placeholder="Email" 
+              name="email" value={formData.email} 
+              onChange={(event) => handleInput('email', event.target.value)} 
+            />
+            <input 
+              type="password" 
+              placeholder="Password" 
+              name="password" 
+              value={formData.password} 
+              onChange={(event) => handleInput('password', event.target.value)} 
+            />
           </>
         )}
         <button type="submit">Submit</button>
