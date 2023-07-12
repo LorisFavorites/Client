7a541cc5dd5950ef233c4750d575425760d30933
M4RT1N <mjlynch35@yahoo.com>
2023-07-12 14:17:55 -0500

    Added cards to product page

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -4,54 +4,42 @@ import { QUERY_INVENTORY } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 
 export default function Products(props) {
-  const { data } = props;
+  // const { data } = props;
   const [cards, setCards] = useState([]);
-  const [splicedCards, setSplicedCards] = useState([]);
-  const cardsArray = [];
-
-  const { loading, inv, error } = useQuery(QUERY_INVENTORY, {
+  const { loading, data, error } = useQuery(QUERY_INVENTORY, {
     variables: { inventory: "pokecards" },
   });
-  console.log("data1", inv);
 
-  useEffect(() => {
-    if (cards) {
-      sortCards();
-    }
-  }, []);
-
-  const sortCards = () => {
-    let sortedCards = data
-      .filter((card) => {
-        if (card.cardmarket) {
-          return card;
-        }
-      })
-      .sort((a, b) => {
-        const aPrice = a.cardmarket.prices.averageSellPrice;
-        const bPrice = b.cardmarket.prices.averageSellPrice;
-
-        if (aPrice < bPrice) {
-          return 1;
-        }
-        if (aPrice > bPrice) {
-          return -1;
-        }
-
-        return 0;
-      });
-    setCards(sortedCards.slice(0, 10));
-    console.log("Cards:", cards);
-  };
-
+  if (loading) {
+    return <Loading />;
+  }
   if (error) {
     console.log(error);
-    return <div>Error</div>;
   }
 
-  const inventories = inv;
+  const cardsData = data.getInventory[0].cards.slice(0, 10);
+  console.log(cardsData);
 
-  console.log("inventories", inventories);
-
-  return <div className="products">Products</div>;
+  return (
+    <div className="products">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-center m-4">
+        {cardsData.map((card, index) => {
+          return (
+            <div
+              key={index}
+              className="bg-gray-200 p-4 h-96"
+              style={{
+                backgroundImage: `url(${card.itemId.images.small})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "contain",
+                backgroundPosition: "center",
+              }}
+            >
+              {card.itemId.name}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
