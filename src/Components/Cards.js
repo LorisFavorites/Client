918d68497e49bea4dc7c57d44f2a475aa88f3da5
M4RT1N <mjlynch35@yahoo.com>
2023-07-12 11:27:30 -0500

    updated products page

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import Loading from "./Loading";
+import { QUERY_INVENTORY } from "../utils/queries";
 
 export default function Cards({ setIsNavOpen }) {
   useEffect(() => {
     setIsNavOpen(false);
   }, []);
 }
-
