5b05aa445441519f977f29e8e553d9f8fa5d0507
Reid M <reidmadock@gmail.com>
2023-07-12 14:19:27 -0500

    fixed inventory query

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,7 +15,7 @@ export default function Header({ setIsNavOpen }) {
   }, []);
 
   useEffect(() => {
-    console.log("Data:", data);
+    // console.log("Data:", data);
     if (data) {
       setIsLoading(false);
       setTestimonials(data.slice(0, 5));
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -4,54 +4,25 @@ import { QUERY_INVENTORY } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 
 export default function Products(props) {
-  const { data } = props;
+  // const { data } = props;
   const [cards, setCards] = useState([]);
-  const [splicedCards, setSplicedCards] = useState([]);
-  const cardsArray = [];
-
-  const { loading, inv, error } = useQuery(QUERY_INVENTORY, {
-    variables: { inventory: "pokecards" },
+  const { loading, error, data } = useQuery(QUERY_INVENTORY, {
+    variables: { inventory: "pokecards" }
   });
-  console.log("data1", inv);
 
-  useEffect(() => {
-    if (cards) {
-      sortCards();
-    }
-  }, []);
+  if (loading) return 'loading...'
+  if (error) return error.message;
+  
+  console.log(data);
 
-  const sortCards = () => {
-    let sortedCards = data
-      .filter((card) => {
-        if (card.cardmarket) {
-          return card;
-        }
-      })
-      .sort((a, b) => {
-        const aPrice = a.cardmarket.prices.averageSellPrice;
-        const bPrice = b.cardmarket.prices.averageSellPrice;
-
-        if (aPrice < bPrice) {
-          return 1;
-        }
-        if (aPrice > bPrice) {
-          return -1;
-        }
-
-        return 0;
-      });
-    setCards(sortedCards.slice(0, 10));
-    console.log("Cards:", cards);
-  };
-
-  if (error) {
-    console.log(error);
-    return <div>Error</div>;
-  }
-
-  const inventories = inv;
-
-  console.log("inventories", inventories);
-
-  return <div className="products">Products</div>;
+  return (
+    <div className="products">
+      {data.getInventory[0].cards.map((card, index) => {
+        return (
+          <div key={index}>
+            {card.itemId.name}
+          </div>)
+      })}
+    </div>
+  );
 }
