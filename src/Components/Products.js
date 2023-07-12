6f3af12c5c03a3df07f68ac5a429c3574b744934
M4RT1N <mjlynch35@yahoo.com>
2023-07-12 15:36:38 -0500

    Added more styling with tailwind

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -17,39 +17,41 @@ export default function Products(props) {
     (card) => card.itemId.cardmarket.prices.trendPrice > 15
   );
 
-  // Slicing filteredCards to only show the first 10 cards
-  const cardsData = filteredCards.slice(0, 18);
+  // Slicing filteredCards to only show the first 16 cards enough for 3 rows while fullscreen
+  const cardsData = filteredCards.slice(0, 16);
 
   return (
-    <div className="products">
-      <h1 className="text-center text-3xl font-bold m-5 underline">
-        Our Products
-      </h1>
-      <br />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 text-center m-5">
-        {cardsData.map((card, index) => {
-          return (
-            <div key={index} className="relative h-128 inset-x-0">
-              <div>
-                <img
-                  src={card.itemId.images.small}
-                  alt="card"
-                  className="absolute left-1/2 -translate-x-2/4 top-0 h-3/4"
-                />
+    <div className="relative w-screen bg-white border-2 h-screen">
+      <div className="products w-2/4 mx-auto h-auto">
+        <h1 className="text-center text-3xl font-bold m-5 underline">
+          Our Products
+        </h1>
+        <br />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 text-center m-5">
+          {cardsData.map((card, index) => {
+            return (
+              <div key={index} className="relative h-128 inset-x-0">
+                <div>
+                  <img
+                    src={card.itemId.images.small}
+                    alt="card"
+                    className="absolute left-1/2 -translate-x-2/4 top-0 h-3/4"
+                  />
+                </div>
+                <div>
+                  <p className="absolute bottom-14 inset-x-0">
+                    ${card.itemId.cardmarket.prices.trendPrice.toFixed(2)}
+                  </p>
+                  <br />
+                  <br />
+                  <button className="absolute bottom-3 left-1/2 -translate-x-2/4 inset-x-0 rounded-none bg-red-600 p-2 text-xs w-6/12">
+                    Add to cart
+                  </button>
+                </div>
               </div>
-              <div>
-                <p className="absolute bottom-14 inset-x-0">
-                  ${card.itemId.cardmarket.prices.trendPrice.toFixed(2)}
-                </p>
-                <br />
-                <br />
-                <button className="absolute bottom-3 left-1/2 -translate-x-2/4 inset-x-0 rounded-none bg-red-600 p-2 text-xs w-6/12">
-                  Add to cart
-                </button>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
