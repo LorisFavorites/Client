a57d0fa988bd76edebfb33309409d60343e96607
Reid M <reidmadock@gmail.com>
2023-07-13 00:50:21 -0500

    swapped to remove favorite button of favorite page

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Card(props) {
-    const { card, index, image, price } = props;
+    const { card, index, image, price, fav } = props;
 
     return (
         <div key={index} className="relative h-128 inset-x-0">
@@ -12,16 +12,24 @@ export default function Card(props) {
                 className="absolute left-1/2 -translate-x-2/4 top-0 h-3/4"
                 />
             </div>
+            {fav ? 
             <div>
-                <p className="absolute bottom-14 inset-x-0">
+                <button className="absolute bottom-3 left-1/2 -translate-x-2/4 inset-x-0 rounded-none bg-red-600 p-2 text-s font-bold w-6/12">
+                    Remove favorite
+                </button>    
+            </div>
+            :
+            <div>
+                <p className="text-amber-50 font-bold absolute bottom-14 inset-x-0">
                 ${ (card.itemId) ? card.itemId.cardmarket.prices.trendPrice.toFixed(2) : price}
                 </p>
                 <br />
                 <br />
-                <button className="absolute bottom-3 left-1/2 -translate-x-2/4 inset-x-0 rounded-none bg-red-600 p-2 text-xs w-6/12">
+                <button className="absolute bottom-3 left-1/2 -translate-x-2/4 inset-x-0 rounded-none bg-red-600 p-2 text-s font-bold w-6/12">
                 Add to cart
                 </button>
-            </div>
+            </div> 
+            }
         </div>
     );
 }
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -22,15 +22,24 @@ export default function Products(props) {
   const cardsData = data.favorites;
 
   return (
-    <div className="relative w-screen bg-white border-2 h-screen">
+    <div className="relative w-screen bg-neutral-800 border-2 h-max">
       <div className="products w-3/4 mx-auto h-auto">
-        <h1 className="text-center text-3xl font-bold m-5 underline">
-          My Favorites
+        <h1 className="text-center text-amber-50 font-bold text-2xl m-5">
+          My <span className="text-red-500">Favorite</span> Cards
+        </h1>
+        <h1 className="text-center text-amber-50 text-2xl m-5">
+          私の<span className="text-red-500">好物</span>カード
         </h1>
         <br />
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-center m-5">
           {cardsData.map((card, index) => {
-            return <Card card={card} index={index} image={card.images.small} price={card.cardmarket.prices.trendPrice} />
+            return <Card 
+              card={card} 
+              index={index} 
+              image={card.images.small} 
+              price={card.cardmarket.prices.trendPrice}
+              fav={true} 
+            />
           })}
         </div>
       </div>
