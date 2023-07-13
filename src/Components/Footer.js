0802f34b80918cc50872adb5a1f4ea35be18d251
AMiro <aj.miro.b@gmail.com>
2023-07-13 12:11:15 -0500

    Add footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,23 @@
-// import React from "react";
-// import "../App.css";
+import React from "react";
+import gitHub from "../assets/github.svg";
+import logo from "../assets/pokefindr-logo.svg"
 
-// <footer class="bg-neutral-950 text-center text-amber-50">
-  
-// </footer>
+const Footer = () => {
+    return (
+        <footer>
+            <div className="foot-container"><img className="logo" src={logo}></img></div>
+            <div className="foot-container"><img className="github" src={gitHub}></img></div>
+            <div className="foot-container">
+                <ul>
+                    <a href="https://github.com/codingprincess111"><li>codingprincess111</li></a>
+                    <a href="https://github.com/ConDuckt"><li>ConDuckt</li></a>
+                    <a href="https://github.com/mjlynch123"><li>mjlynch123</li></a>
+                    <a href="https://github.com/reidmadock"><li>reidmadock</li></a>
+                    <a href="https://github.com/ToastyStudent"><li>ToastyStudent</li></a>
+                </ul>
+            </div>    
+        </footer>
+    );
+};
+
+export default Footer;
