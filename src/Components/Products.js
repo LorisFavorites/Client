1c1238ccb057b7ee6b01ed08966f8fb13e0d2c3f
AMiro <aj.miro.b@gmail.com>
2023-07-12 23:17:13 -0400

    Rework Products.js styling

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -23,10 +23,10 @@ export default function Products(props) {
   const cardsData = filteredCards.slice(0, 15);
 
   return (
-    <div className="relative w-screen bg-white border-2 h-screen">
+    <div className="relative w-screen bg-neutral-800 border-2 h-max">
       <div className="products w-3/4 mx-auto h-auto">
-        <h1 className="text-center text-3xl font-bold m-5 underline">
-          Our Products
+        <h1 className="text-center text-amber-50 text-2xl font-bold m-5">
+        █████ <span className="text-red-500">█████</span> ███ ████ | ポケモンゲットだぜ! 
         </h1>
         <br />
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-center m-5">
@@ -37,16 +37,16 @@ export default function Products(props) {
                   <img
                     src={card.itemId.images.small}
                     alt="card"
-                    className="absolute left-1/2 -translate-x-2/4 top-0 h-3/4"
+                    className="absolute left-1/2 -translate-x-2/4 top-0 h-3/4 rounded-md"
                   />
                 </div>
                 <div>
-                  <p className="absolute bottom-14 inset-x-0">
+                  <p className="text-amber-50 font-bold absolute bottom-14 inset-x-0">
                     ${card.itemId.cardmarket.prices.trendPrice.toFixed(2)}
                   </p>
                   <br />
                   <br />
-                  <button className="absolute bottom-3 left-1/2 -translate-x-2/4 inset-x-0 rounded-none bg-red-600 p-2 text-xs w-6/12">
+                  <button className="absolute rounded-md bottom-3 left-1/2 -translate-x-2/4 inset-x-0 bg-red-500 hover:bg-red-900 hover:text-amber-50 p-2 text-xs w-6/12">
                     Add to cart
                   </button>
                 </div>
